47580efe9169bc21cd1fa63dbb8439a4b9543016
Dvnhrdt <harditiadovan@gmail.com>
2021-10-13 02:08:09 +0700

    first commit

---
+++ challenge.java
@@ -0,0 +1,35 @@
+public class challenge {
+    
+    
+    public static void main(String[]args) {
+        String firstName = "Dovan ";
+        String lastName = "Harditia";
+        String addreas = "Bukittinggi";
+        String placeofbirth = "Padang Panjang";
+        int dateofbirth = 03-03-1999;
+
+        String phoneNumber = "081277752619";
+        Integer age = 22;
+        String hobby = "Sepakbola";
+
+
+
+    
+
+
+
+
+        System.out.println(firstName + lastName);
+        System.out.println( addreas);
+        System.out.println(placeofbirth + dateofbirth);
+        System.out.println(phoneNumber);
+        System.out.println(age);
+        System.out.println(hobby);
+        
+
+
+    }
+        
+    
+
+}
